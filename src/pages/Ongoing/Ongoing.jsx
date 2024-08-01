55d5c86bd6bba5aec98b9eb64f65adfcf22d34c1
ZannMv <116694002+ZannMv@users.noreply.github.com>
2024-08-01 18:10:54 +0700

    Update Ongoing.jsx

--- src/pages/Ongoing/Ongoing.jsx
+++ src/pages/Ongoing/Ongoing.jsx
@@ -27,29 +27,29 @@ const Ongoing = () => {
     return(
         <>
             <Helmet>
-                <title>Ongoing Anime - Kumanime</title>
+                <title>Ongoing Anime - Zlanime</title>
                 <meta
                 name="description"
-                content="Nonton anime subtitle Indonesia secara gratis di KUMANIME.FUN"
+                content="Nonton anime subtitle Indonesia secara gratis di ZLANIME.VERCEL.APP"
                 />
                 <meta
                 property="og:title"
-                content="Ongoing Anime - Kumanime"
+                content="Ongoing Anime - Zlanime"
                 />
                 <meta name="robots" content="index, follow" />
                 <meta
                 property="og:description"
-                content="Nonton anime subtitle Indonesia secara gratis di KUMANIME.FUN"
+                content="Nonton anime subtitle Indonesia secara gratis di ZLANIME.VERCEL.APP"
                 />
                 <meta property="og:locale" content="id_ID" />
                 <meta property="og:type" content="article" />
-                <meta property="og:site_name" content="Kumanime" />
+                <meta property="og:site_name" content="Zlanime" />
                 <meta property="og:image" content="https://raw.githubusercontent.com/MastayY/kumanime/main/public/logo.png" />
                 <meta name="googlebot" content="index, follow" />
-                <meta name="twitter:title" content="Ongoing Anime - Kumanime" />
+                <meta name="twitter:title" content="Ongoing Anime - Zlanime" />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="keywords" content="kumanime, otakudesu, kuronime, kuramanime, web streaming anime, moenime, moenime id, moenime list, moe anime, anime batch indonesia, anime batch sub indo, animebatch sub indo, anime batch terbaru, download anime batch subtitle indonesia, situs download anime, anime sub indo, download anime sub indo, download anime subtitle indonesia, download anime terbaru, download anime bd, download anime movie, download anime batch, download anime batch sub indo, download anime batch subtitle indonesia terlengkap, streaming anime, streaming anime sub indo, streaming anime subtitle indonesia, streaming anime sub indo lengkap" />
-                <meta name="twitter:description" content="Nonton Anime Online Sub Indo Gratis di KUMANIME.FUN" />
+                <meta name="keywords" content="zlanime, otakudesu, kuronime, kuramanime, web streaming anime, moenime, moenime id, moenime list, moe anime, anime batch indonesia, anime batch sub indo, animebatch sub indo, anime batch terbaru, download anime batch subtitle indonesia, situs download anime, anime sub indo, download anime sub indo, download anime subtitle indonesia, download anime terbaru, download anime bd, download anime movie, download anime batch, download anime batch sub indo, download anime batch subtitle indonesia terlengkap, streaming anime, streaming anime sub indo, streaming anime subtitle indonesia, streaming anime sub indo lengkap" />
+                <meta name="twitter:description" content="Nonton Anime Online Sub Indo Gratis di ZLANIME.VERCEL.APP" />
                 <meta
                 name="twitter:image"
                 content="https://raw.githubusercontent.com/MastayY/kumanime/main/public/logo.png"
